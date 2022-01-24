164889dedb3f799684305d454d87515018993656
sachin-source <55872747+sachin-source@users.noreply.github.com>
2022-01-24 15:49:54 +0530

    Close sidebar on small screens

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 let menu = document.querySelector("#menu-btn")
 let header = document.querySelector(".header")
 
-menu.onclick = () => {
+
+const sidebarDisplay = () => {
     menu.classList.toggle('fa-times')
     header.classList.toggle('active')
 }
+
+menu.onclick = sidebarDisplay;
+
 let themeToggler = document.querySelector("#theme-toggler")
 themeToggler.onclick = ()=> {
     //var _ = (themeToggler.classList.toggle('fa-sun') ? themeToggler.classList.add('active')) : themeToggler.classList.remove('active')
